5c87b5997f31145003e36850be3ca5dfef273dbe
Newar8519 <58194569+Newar8519@users.noreply.github.com>
2020-01-03 10:53:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<title>One Hour Coding</title>
+<title> One Hour Coding </title>
+
+<ul>
+  <li><a href="default.asp">Home</a></li>
+  <li><a href="news.asp">News</a></li>
+  <li><a href="contact.asp">Contact</a></li>
+  <li><a href="about.asp">About</a></li>
+</ul>
+
+<h1> This is an hour of code, and what I have learned. </h1>
+<p> All of the coding in this website has been learned from, <a href="https://www.w3schools.com/html">w3schools.com</a>
