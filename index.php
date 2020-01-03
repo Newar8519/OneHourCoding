12bfbce080ce2dc3e14e7dfc375fda634e9e6bfe
Newar8519 <58194569+Newar8519@users.noreply.github.com>
2020-01-03 10:57:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,41 @@
 <title> One Hour Coding </title>
 
+<head>
+<style>
+body {margin:0;}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+li {
+  float: left;
+}
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+li a:hover:not(.active) {
+  background-color: #111;
+}
+.active {
+  background-color: #4CAF50;
+}
+</style>
+</head>
+
 <ul>
-  <li><a href="default.asp">Home</a></li>
-  <li><a href="news.asp">News</a></li>
-  <li><a href="contact.asp">Contact</a></li>
-  <li><a href="about.asp">About</a></li>
+  <li><a classhref="/index.html">Home</a></li>
+  <li><a class="active" href="/Html.html">Html</a></li>
+  <li><a classhref="/about.html">About</a></li>
 </ul>
 
 <h1> This is an hour of code, and what I have learned. </h1>
