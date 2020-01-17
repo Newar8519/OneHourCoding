ce19b350d8023a9bdd2d09a2d7b4d88c4e7df75d
Newar8519 <58194569+Newar8519@users.noreply.github.com>
2020-01-16 22:13:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,4 @@
-<title> One Hour Coding </title>
-
-<head>
-<style>
-body {margin:0;}
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-  position: fixed;
-  top: 0;
-  width: 100%;
-}
-li {
-  float: left;
-}
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-li a:hover:not(.active) {
-  background-color: #111;
-}
-.active {
-  background-color: #4CAF50;
-}
-</style>
-</head>
-
-<ul>
-  <li><a class="active" href="/index.html">Home</a></li>
-  <li><a classhref="/Html.html">Html</a></li>
-  <li><a classhref="/about.html">About</a></li>
-</ul>
-
-<h1> This is an hour of code, and what I have learned. </h1>
-<p> All of the coding in this website has been learned from, <a href="https://www.w3schools.com/html">w3schools.com</a>
+<meta charset="utf-8">
+<title>Redirecting to ERROR/</title>
+<meta http-equiv="refresh" content="0; URL=https://onehourofcoding.herokuapp.com/404.html">
+<link rel="canonical" href="https://onehourofcoding.herokuapp.com/404.html">
