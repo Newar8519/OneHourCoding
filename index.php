58182bd8c4bc2ae9728fbc33b60fc73c89a186fe
Newar8519 <58194569+Newar8519@users.noreply.github.com>
2020-01-16 22:14:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <meta charset="utf-8">
 <title>Redirecting to ERROR/</title>
-<meta http-equiv="refresh" content="0; URL=https://onehourofcoding.herokuapp.com/404.html">
-<link rel="canonical" href="https://onehourofcoding.herokuapp.com/404.html">
+<meta http-equiv="refresh" content="0; URL=https://teamtreesbestie.herokuapp.com/404.html">
+<link rel="canonical" href="https://teamtreesbestie.herokuapp.com/404.html">
